84fdc9991d8dee431adc62ca8d28042056681c24
neoxr <neoxr@null.net>
2023-07-21 11:10:40 +0700

    +

--- system/functions.js
+++ system/functions.js
@@ -1096,4 +1096,27 @@ module.exports = class Function {
          resolve(ab)
       })
    }
+   
+   /* Text Style
+    * @param {String} text
+    * @param {Integer} style
+    */
+   Styles = (text, style = 1) => {
+   var xStr = 'abcdefghijklmnopqrstuvwxyz1234567890'.split('')
+   var yStr = Object.freeze({
+      1: 'ᴀʙᴄᴅᴇꜰɢʜɪᴊᴋʟᴍɴᴏᴘqʀꜱᴛᴜᴠᴡxʏᴢ1234567890'
+   })
+   var replacer = []
+   xStr.map((v, i) => replacer.push({
+      original: v,
+      convert: yStr[style].split('')[i]
+   }))
+   var str = text.toLowerCase().split('')
+   var output = []
+   str.map(v => {
+      const find = replacer.find(x => x.original == v)
+      find ? output.push(find.convert) : output.push(v)
+   })
+   return output.join('')
+}
 }
