d3e68a9dbdb361aa16a0f7572c9be561153f43be
neoxr <neoxr@null.net>
2023-08-28 10:20:24 +0700

    render group

--- system/logs.js
+++ system/logs.js
@@ -4,15 +4,16 @@ const moment = require('moment-timezone')
 moment.tz.setDefault('Asia/Jakarta').locale('id')
 const Func = new (require('./functions'))
 
-module.exports = async (client, m, myPrefix, type = 1) => {
+module.exports = async (client, m, myPrefix, groupName = false, type = 1) => {
    let who = m.fromMe ? 'Self' : m.pushName || 'No Name'
    let time = m.messageTimestamp   
    let pc = false
    if (m.chat.endsWith('@s.whatsapp.net')) pc = true
    if (!Func.mtype(m)) return
    if (!pc) {
-      if (typeof m.text != 'object' && m.text.startsWith(myPrefix)) return console.log('\n' + yellow.bold('[ CMD ]'), Func.color(moment(time * 1000).format('DD/MM/YY HH:mm:ss'), 'green'), gray.bgGreen(' ' + m.mtype + ' '), green.bold('from'), '[' + m.sender.split`@` [0] + '] ' + gray.bgYellow(' ' + who + ' '), Func.color('in'), '[' + m.chat + '] ' + gray.bgYellow(' ' + await (await client.groupMetadata(m.chat)).subject + ' '), `\n${Func.mtype(m)}`)
-      if (type == 1) console.log('\n' + blueBright.bold('[ MSG ]'), Func.color(moment(time * 1000).format('DD/MM/YY HH:mm:ss'), 'green'), gray.bgGreen(' ' + m.mtype + ' '), green.bold('from'), '[' + m.sender.split`@` [0] + '] ' + gray.bgYellow(' ' + who + ' '), Func.color('in'), '[' + m.chat + '] ' + gray.bgYellow(' ' + await (await client.groupMetadata(m.chat)).subject + ' '), `\n${Func.mtype(m)}`)
+      const group = groupName ? gray.bgYellow(' ' + await (await client.groupMetadata(m.chat)).subject + ' ') : '******'
+      if (typeof m.text != 'object' && m.text.startsWith(myPrefix)) return console.log('\n' + yellow.bold('[ CMD ]'), Func.color(moment(time * 1000).format('DD/MM/YY HH:mm:ss'), 'green'), gray.bgGreen(' ' + m.mtype + ' '), green.bold('from'), '[' + m.sender.split`@` [0] + '] ' + gray.bgYellow(' ' + who + ' '), Func.color('in'), '[' + m.chat + '] ' + group, `\n${Func.mtype(m)}`)
+      if (type == 1) console.log('\n' + blueBright.bold('[ MSG ]'), Func.color(moment(time * 1000).format('DD/MM/YY HH:mm:ss'), 'green'), gray.bgGreen(' ' + m.mtype + ' '), green.bold('from'), '[' + m.sender.split`@` [0] + '] ' + gray.bgYellow(' ' + who + ' '), Func.color('in'), '[' + m.chat + '] ' + group, `\n${Func.mtype(m)}`)
    } else if (pc) {
       if (typeof m.text != 'object' && m.text.startsWith(myPrefix)) return console.log('\n' + yellow.bold('[ CMD ]'), Func.color(moment(time * 1000).format('DD/MM/YY HH:mm:ss'), 'green'), gray.bgGreen(' ' + m.mtype + ' '), green.bold('from'), '[' + m.sender.split`@` [0] + '] ' + gray.bgYellow(' ' + who + ' '), Func.color('in'), '[' + m.chat + ']', `\n${Func.mtype(m)}`)
       if (type == 1) console.log('\n' + blueBright.bold('[ MSG ]'), Func.color(moment(time * 1000).format('DD/MM/YY HH:mm:ss'), 'green'), gray.bgGreen(' ' + m.mtype + ' '), green.bold('from'), '[' + m.sender.split`@` [0] + '] ' + gray.bgYellow(' ' + who + ' '), Func.color('in'), '[' + m.chat + ']', `\n${Func.mtype(m)}`)
