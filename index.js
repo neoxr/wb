41d6179a9014b15a3acffc9b7d79ad3a3c160b21
neoxr <admin@neoxr.my.id>
2025-01-06 13:43:24 +0000

    chore: improve

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 require('dotenv').config()
 const fs = require('fs')
-module.exports = class Component {
-   Config = JSON.parse(fs.readFileSync('./config.json'))
-   Client = require('./Utils/_Connection')
-   Baileys = require('./Utils/Connection')
-   Converter = new (require('./Utils/Converter'))
-   Function = new (require('./Utils/Function'))
-   Scraper = new (require('./Utils/Scraper'))
-   Scandir = new (require('./Utils/Loader'))
-   Message = require('./Utils/Message')
-   Cloud = require('./Utils/_Store')
-   InvCloud = require('./Utils/Store')
-   Logs = require('./Utils/Logs')
-   NeoxrCommands = new(require('./Utils/Cmd'))
-   NeoxrApi = require('@neoxr/api')
-   Cooldown = require('./Utils/Cooldown')
-   Queue = require('./Utils/Queue')
-   Spam = require('./Utils/Spam')
-   Session = require('./Utils/Session')
-   Connector = require('./Utils/Connector')
+
+// No Locked can be manipulated >_<
+const Function = new (require('./Utils/Function'))
+const Scraper = new (require('./Utils/Scraper'))
+
+class Component {
+   constructor() {
+      this.Config = JSON.parse(fs.readFileSync('./config.json'))
+      this.Client = require('./Utils/_Connection')
+      this.Baileys = require('./Utils/Connection')
+      this.Converter = new (require('./Utils/Converter'))
+      this.Function = Function
+      this.Scraper = Scraper
+      this.Scandir = new (require('./Utils/Loader'))
+      this.Message = require('./Utils/Message')
+      this.Cloud = require('./Utils/_Store')
+      this.InvCloud = require('./Utils/Store')
+      this.Logs = require('./Utils/Logs')
+      this.NeoxrCommands = new (require('./Utils/Cmd'))
+      this.NeoxrApi = require('@neoxr/api')
+      this.Cooldown = require('./Utils/Cooldown')
+      this.Queue = require('./Utils/Queue')
+      this.Spam = require('./Utils/Spam')
+      this.Session = require('./Utils/Session')
+      this.Connector = require('./Utils/Connector')
+   }
 }
+
+module.exports = Component
