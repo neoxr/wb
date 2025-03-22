db03829df9806408022aa54ffb6eb91b066f3639
neoxr <admin@neoxr.my.id>
2025-03-22 06:49:15 +0000

    +

--- lib/Utils/spam.d.ts
+++ lib/Utils/spam.d.ts
@@ -1,8 +1,6 @@
+import { User, DetectionOptions } from './type';
 export default class SpamDetector {
-    users: {
-        _id: string;
-        spam_point: number;
-    }[];
+    users: User[];
     RESET_TIMER: number;
     HOLD_TIMER: number;
     PERMANENT_THRESHOLD: number;
@@ -15,20 +13,9 @@ export default class SpamDetector {
         NOTIFY_THRESHOLD?: number;
         BANNED_THRESHOLD?: number;
     });
-    pushId: (id: string) => number;
-    peek: (id: string) => {
-        _id: string;
-        spam_point: number;
-    } | undefined;
-    detection: (client: any, m: any, options?: {
-        coldown?: any;
-        users?: any;
-        prefix?: string;
-        command?: string;
-        commands?: string[];
-        show?: string;
-        banned_times?: number;
-    }) => {
+    pushId: (id: string) => void;
+    peek: (id: string) => User | undefined;
+    detection: (client: any, m: any, options: DetectionOptions) => {
         state: string;
         msg?: undefined;
     } | {
--- lib/Utils/type.d.ts
+++ lib/Utils/type.d.ts
@@ -19,3 +19,24 @@ export interface Bot {
     jid: string;
     data: any;
 }
+export interface User {
+    _id: string;
+    spam_point: number;
+}
+export interface DetectionOptions {
+    prefix: string;
+    command: string;
+    commands: string[];
+    show: boolean;
+    users: {
+        ban_temporary: number;
+        banned: boolean;
+        ban_times: number;
+    };
+    banned_times: number;
+    cooldown: {
+        hold: (chat: string) => {
+            state: boolean;
+        };
+    };
+}
