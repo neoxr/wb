f50c7b12a6aa45cc321114d2468f85c0e871c317
neoxr <neoxr@null.net>
2025-04-04 18:38:39 +0000

    update

--- lib/Utils/session.d.ts
+++ lib/Utils/session.d.ts
@@ -1,10 +1,11 @@
 import 'rootpath';
 declare class Session {
-    constructor();
+    session: any;
+    BACKUP_DIR: string;
+    constructor(session: any);
     init: () => void;
     backup: (client: any, dir: string) => Promise<void>;
     restore: (client: any, dir: string) => Promise<void>;
     isBackupExist: (client: any) => Promise<boolean>;
 }
-declare const _default: Session;
-export default _default;
+export default Session;
