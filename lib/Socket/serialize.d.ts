7046c456777b33533aa467e39a6d56ffe364c1a6
neoxr <neoxr@null.net>
2025-05-25 19:15:52 +0000

    chore(release): 5.7.19

--- lib/Socket/serialize.d.ts
+++
@@ -1,8 +0,0 @@
-import Message from './message';
-declare class Serialize extends Message {
-    mention: (text: string) => string[];
-    jidNormalize: (parent: any, jid: string) => string;
-    bind(parent: any, m: any, bypassDisappearing?: boolean, bot?: (id: string) => boolean): any;
-}
-declare const _default: Serialize;
-export default _default;
