3707b9e4107a546002dec49ebd4fb429d93271e2
neoxr <neoxr@null.net>
2025-06-14 01:55:04 +0000

    chore(release): 5.9.4

--- lib/Utils/queue.d.ts
+++
@@ -1,9 +0,0 @@
-export default class Queue {
-    items: any[];
-    delay: number;
-    constructor(delay: number);
-    enqueue: (item: any) => number;
-    dequeue: () => void;
-    peek: (access?: boolean) => Promise<any>;
-    count: () => number;
-}
