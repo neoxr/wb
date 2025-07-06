715d49828b7290ffae4f9ee8f320fe22531c4923
neoxr <neoxr@null.net>
2025-07-06 08:17:02 +0000

    chore(release): 5.10.7

---
+++ lib/Utils/jid-helper.d.ts
@@ -0,0 +1,17 @@
+declare const _default: (client?: any) => {
+    hostJid: boolean | undefined;
+    clientJid: string | undefined;
+    findJid: {
+        bot: (jid: string) => any;
+        group: (jid: string) => any;
+        chat: (jid: string) => any;
+        user: (jid: string) => any;
+    } | undefined;
+    bot: {
+        jid: string;
+        socket: any;
+    }[];
+    getbot: (jid: string) => any;
+    setbot: (jid: string, socket: any) => number;
+};
+export default _default;
