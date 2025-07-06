c5fd50a6464562e77efbf1bff7b1b35a9bc1009b
neoxr <neoxr@null.net>
2025-07-06 08:31:48 +0000

    chore(release): 5.10.7

--- lib/Utils/converter.d.ts
+++
@@ -1,9 +0,0 @@
-declare class Converter {
-    ffmpeg: (buffer: Buffer, args?: string[], ext?: string, ext2?: string) => Promise<Buffer>;
-    toAudio: (buffer: Buffer, ext: string) => Promise<Buffer>;
-    toPTT: (buffer: Buffer, ext: string) => Promise<Buffer>;
-    toVideo: (buffer: Buffer, ext: string) => Promise<Buffer>;
-    toMp3: (inputFile: Buffer | string) => Promise<Buffer>;
-}
-declare const _default: Converter;
-export default _default;
